71d74eec999af82740d372019cced0de5e8b4ccd
Ayush Agarwal <agarwal.ayush.0109@gmail.com>
2021-07-24 02:00:13 +0530

    Minor change

--- socketControllers/index.js
+++ socketControllers/index.js
@@ -40,7 +40,7 @@ module.exports = (io, socket, teamId, roomId) => {
       pos: data.pos,
       teamId: teamId,
     });
-    // await team.save(); //!
+    await team.save(); //!
 
     //! wait for save?
     // check for not allowed
