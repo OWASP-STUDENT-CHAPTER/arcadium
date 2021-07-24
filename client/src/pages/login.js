5be0ec6d9a86db0a70e3ca3569b091c521a5597f
Ayush Agarwal <agarwal.ayush.0109@gmail.com>
2021-07-24 16:40:22 +0530

    Login Page UI added

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,33 @@
 import URL from '../util/URL';
+import '../assets/css/Login.css';
 
 const Login = () => {
   return (
     <div className='login-component'>
-      <a href={`${URL}/api/auth/login`}>LOGIN</a>
+      <div className='wrapper'>
+        <form>
+          <label className='off label'>
+            Click on the switch to <span>Login!</span>
+          </label>
+          <a href={`${URL}/api/auth/login`}>
+            <div className='switch-wrapper'>
+              <div className='switch-overlay'></div>
+              <div className='switch-top'>
+                <input
+                  type='checkbox'
+                  name='switch'
+                  className='button'
+                  checked='checked'
+                />
+              </div>
+              <div className='light on'></div>
+              <div className='light-off off'></div>
+              <div className='switch-bottom'></div>
+            </div>
+          </a>
+          <label className='on switch-on label'></label>
+        </form>
+      </div>
     </div>
   );
 };
